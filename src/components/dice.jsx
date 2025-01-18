6ed0f215ed6f4df4b7779f2b3c06e76e1a3f4956
YashaswiniMM27 <yashaswinimm2703@gmail.com>
2025-01-18 18:31:36 +0530

    finshed tenzies

--- src/components/dice.jsx
+++ src/components/dice.jsx
@@ -1,31 +1,57 @@
 import React from "react";
 import Die from "./die";
+import { nanoid } from "nanoid"
+import ReactConfetti from "react-confetti";
 
 export default function Dice(){
 
-    const [dice, setDice] = React.useState(generateAllNewDice())
+    const [dice, setDice] = React.useState(() => generateAllNewDice())
+
+    const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
     function generateAllNewDice(){
-        const newDice = []
-        for(let i =0 ; i < 10; i++){
-            const randomDie = (Math.floor(Math.random() * 6) + 1);
-            newDice.push(randomDie)
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
         }
-        return newDice
-    }
+
 
     function rollDice(){
-        setDice(generateAllNewDice())
+        if(!gameWon){
+            setDice(oldDice => oldDice.map((die) =>
+                die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6) }
+            ))
+        }else{
+            setDice(generateAllNewDice())
+        }
+        
     }
 
-    const diceElements = dice.map((num, index) => <Die key={index} value={num}/>)
+    function hold(id){
+        setDice(oldDice => oldDice.map((die) =>
+                                die.id === id ? {...die, isHeld: !die.isHeld} : die))
+    }
+
+
+    const diceElements = dice.map((die) => <Die
+                                                key={die.id}
+                                                value={die.value}
+                                                isHeld={die.isHeld}
+                                                hold={hold}
+                                                id={die.id}
+                                            />)
 
     return(
         <section className="mainDiceSection">
+            {gameWon && <ReactConfetti/>}
             <section className="diceSection">
                 {diceElements}
             </section>
-            <button className="rollButton" onClick={rollDice}>ROLL</button>
+            <button className="rollButton" onClick={rollDice}>{gameWon? "NEW GAME" : "ROLL"}</button>
         </section>
     )
 }
--- src/components/die.jsx
+++ src/components/die.jsx
@@ -1,7 +1,12 @@
 import '../index.css'
 
-export default function Die({ value }){
+export default function Die({ value, isHeld, hold, id }){
+
+    const heldStyle = {
+        backgroundColor: isHeld ? " #59E391" : "white"
+    }
+
     return (
-        <button className="diceButton" >{value}</button>
+        <button className="diceButton" style={heldStyle} onClick={() => hold(id)}>{value}</button>
     )
 }
